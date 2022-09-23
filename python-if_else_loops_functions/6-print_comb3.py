be630a211b86174c2ee608ec62b45968bfa7c64b
MiguelGit20 <5178@holbertonschool.com>
2022-09-23 10:58:46 -0700

    all possible different combinations of two digits.

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,7 @@ a = 0
 for i in range(10):
     if i == 8:
         print("{}{}".format(i, j))
+        break
     for j in range(10):
         if j > a:
             print("{}{}".format(i, j), end=", ")
