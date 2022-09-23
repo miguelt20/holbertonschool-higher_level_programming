67115c8f29cfe3be1c14b6ea3a2271a19e162228
MiguelGit20 <5178@holbertonschool.com>
2022-09-23 09:42:39 -0700

    My first toolbox!

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-from calculator_1 import add,sub,mul,div
+from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
-if __name__ == '__main__': 
+if __name__ == '__main__':
     print("{} + {} = {}".format(a, b, add(a, b)))
 if __name__ == '__main__':
     print("{} - {} = {}".format(a, b, sub(a, b)))
