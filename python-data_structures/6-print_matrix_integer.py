4b40cc0fcd515ad9eb7babcb2862b2b2af74ed6e
MiguelGit20 <5178@holbertonschool.com>
2022-09-23 20:30:45 -0700

    prints a matrix of integers

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,6 @@
+#1/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for val in row:
+             print("{:d}".format(val), end=" " if val != row[-1] else "")
+        print()
