4807c8195c5281a6e401de03dde6cb7fb42b47aa
MiguelGit20 <5178@holbertonschool.com>
2022-09-26 14:48:51 -0700

    Simple delete by key

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
     for keys in a_dictionary:
-        rm = a_dictionary.pop(key, None)
-        if len(a_dictionary) == 0:
-            return
-        elif rm is None:
-            return a_dictionary
+        if a_dictionary is None:
+            return None
         else:
-            return a_dictionary
+            rm = a_dictionary.pop(key, None)
+            if rm is None:
+                return a_dictionary
+            else:
+                return a_dictionary
