def77682d94588b5b87e5dc06bb3ada6b1487d87
MiguelGit20 <5178@holbertonschool.com>
2022-09-26 09:55:00 -0700

    Squared simple

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,9 +2,12 @@
 def power(num):
     num = num ** 2
     return num
+
+
 def square_matrix_simple(matrix=[]):
+    counter = 0
     new_matrix = []
-    new_matrix.append(list(map(power, matrix[0])))
-    new_matrix.append(list(map(power, matrix[1])))
-    new_matrix.append(list(map(power, matrix[2])))
+    for row in matrix:
+        new_matrix.append(list(map(power, matrix[counter])))
+        counter = counter + 1
     return new_matrix
