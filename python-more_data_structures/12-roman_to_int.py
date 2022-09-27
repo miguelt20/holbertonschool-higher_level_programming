4a226dfbdd071617550cf5a7e9d056e9a3a3a0d4
MiguelGit20 <5178@holbertonschool.com>
2022-09-27 08:36:50 -0700

    Roman to Integer

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    numbers = {'I': 1,'V': 5, 'X': 10, 'L':50, 'C': 100, 'D':500, 'M':1000}
+    numbers = {'I': 1, 'V': 5, 'X': 10, 'L':50, 'C': 100, 'D':500, 'M':1000}
     decimal = 0
-    if roman_string is None:
-        return 0
-    else:
-        for char in roman_string:
-            if roman_string == 'IX':
-                decimal = 9
-            else:
-                for i in numbers:
-                    if char == i:
-                        decimal = decimal + numbers[i]
-        return decimal
+    for i in range(len(roman_string)):
+        if i > 0 and numbers[roman_string[i]] > numbers[roman_string[i - 1]]:
+            decimal += numbers[roman_string[i]] - 2 * numbers[roman_string[i - 1]]
+        else:
+            decimal += numbers[roman_string[i]]
+    return decimal
