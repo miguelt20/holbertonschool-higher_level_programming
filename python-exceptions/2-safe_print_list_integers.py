74228cc59d6a676bdf0ed13f52dd5afd94451e8f
MiguelGit20 <miguel.torres.20@hotmail.com>
2022-09-29 20:06:15 -0500

    Print and count integers

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    counter = 0
+    for i in my_list:
+        try:
+            if i >= 0 and i <= 9:
+                if counter < x:
+                    print(i, end="")
+                    counter += 1
+        except:
+            print("", end="")
+    print()
+    return counter
