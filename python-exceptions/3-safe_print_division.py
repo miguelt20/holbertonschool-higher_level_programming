f30c23161b66ad6f6920e86503b7258376a8a58b
MiguelGit20 <miguel.torres.20@hotmail.com>
2022-09-29 23:44:52 -0500

    Integers division with debug

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        return a / b
+    except:
+        print("Inside result: None")
+        return None
+    finally:
+        try:
+            print("Inside result: {}".format(a/b))
+        except:
+            return None
+        
