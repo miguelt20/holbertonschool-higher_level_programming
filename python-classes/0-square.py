c0a66df95b2533646961be859d3e851450bd40cc
MiguelGit20 <miguel.torres.20@hotmail.com>
2022-09-30 21:02:02 -0500

    empty square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-"""
-python3 -c 'print(__import__("0-square").__doc__)'
-
-"""
-
-""""
-python3 -c 'print(__import__("0-square").Square.__doc__)'
-"""
 class Square:
+    """ Empty Square """   
     pass
