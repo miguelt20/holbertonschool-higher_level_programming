65c523198023ca0778b9de62020959c988297501
MiguelGit20 <miguel.torres.20@hotmail.com>
2022-09-30 20:58:09 -0500

    square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """
 python3 -c 'print(__import__("0-square").__doc__)'
-python3 -c 'print(__import__("0-square").Square.__doc__)'
 
 """
 
-
+""""
+python3 -c 'print(__import__("0-square").Square.__doc__)'
+"""
 class Square:
     pass
