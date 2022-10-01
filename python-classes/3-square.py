f5ee03c8191a42f9cbe1053b3ace79083f958db1
MiguelGit20 <miguel.torres.20@hotmail.com>
2022-09-30 22:45:28 -0500

    Area of a square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -6,7 +6,7 @@ class Square:
     """ Private instance attribute: size
         size must be an integer
         size must be greater than 0 """
-    def __init__(self, size):
+    def __init__(self, size=0):
         self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
@@ -15,5 +15,8 @@ class Square:
 
     """ Public instance method that returns the current square area"""
     def area(self):
-        area = self.__size ** 2
+        if self.__size == 0:
+            return 0
+        else: 
+            area = self.__size ** 2
         return area
