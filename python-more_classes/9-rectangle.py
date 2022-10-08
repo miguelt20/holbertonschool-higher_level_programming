a43625ad7f751229e21a20603fc64411c56d35d1
MiguelGit20 <miguel.torres.20@hotmail.com>
2022-10-07 22:06:26 -0500

    square is a rectangle

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -10,8 +10,8 @@ class Rectangle:
 
     """ Init Method """
     def __init__(self, width=0, height=0):
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     """ property and setter for width """
