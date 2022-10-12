4c124d9b05a0c926c130e5682b4979d2bb5c8045
MiguelGit20 <miguel.torres.20@hotmail.com>
2022-10-12 15:36:00 -0500

    class is documented

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-""" Module that contains a Class """
+"""  
+Module that contains a 
+class MyList that inherits from list
+"""
 
 
 class MyList(list):
+    """ Class that inherits from list """
 
     def print_sorted(self):
         print(sorted(self))
