c8028bd02ea292fa7182a16f2e968aec4a5ad7d8
MiguelGit20 <miguel.torres.20@hotmail.com>
2022-10-13 13:09:00 -0500

    Improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module that contains a class BaseGeometry """
+
+
+class BaseGeometry:
+    """ class BaseGeometry """
+    
+    def area(self):
+        raise Exception("area() is not implemented")
