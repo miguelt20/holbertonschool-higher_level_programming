f47c63ad6d2261fc20cd4c27b1211e2d66b7b81f
MiguelGit20 <miguel.torres.20@hotmail.com>
2022-10-14 18:40:38 -0500

    Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,5 +6,7 @@ that reads a text file (UTF8) and prints it to stdout
 
 
 def read_file(filename=""):
+    """ Functions that reads a file """
+
     with open(filename, encoding="utf-8") as f:
         print(f.read())
