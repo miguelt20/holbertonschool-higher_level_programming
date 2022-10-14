fd3a49ca2968bb2f1555b545fb1f2458e01a3106
MiguelGit20 <miguel.torres.20@hotmail.com>
2022-10-14 15:15:53 -0500

    Square #2

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Module that contains a class Square """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Class Square """
+
+    """ Init Method """
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    """ Area Method """
+
+    def area(self):
+        return self.__size * self.__size
+
+    """ Str Method """
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
