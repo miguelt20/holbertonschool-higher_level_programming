7ae62b5a0c5246e3f2b2e805f6619efe76ec03c9
MiguelGit20 <miguel.torres.20@hotmail.com>
2022-10-16 16:54:12 -0500

    Student to JSON

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -11,8 +11,8 @@ class Student:
     """ Constructor """
 
     def __init__(self, first_name, last_name, age):
-        self.f_name = first_name
-        self.l_name = last_name
+        self.first_name = first_name
+        self.last_name = last_name
         self.age = age
 
     """ Json Method """
