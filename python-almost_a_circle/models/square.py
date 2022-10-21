b13d2099ccf3acedf67ab820aa666fe9dca84965
MiguelGit20 <miguel.torres.20@hotmail.com>
2022-10-21 14:08:10 -0500

    Square size

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -11,9 +11,27 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """ Constructor method """
-        super().__init__(size, size, x, y, id)
+
         self.size = size
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """ Str method """
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.__width}"
+
+    @property
+    def size(self):
+        """ Getter """
+
+        return self.__width
+
+    @size.setter
+    def size(self, value):
+        """ Setter """
+
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+        self.__height = value
