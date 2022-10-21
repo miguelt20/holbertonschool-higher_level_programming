5891fa2af3fe34d0376ed5ef1f13e139d4ee15f8
MiguelGit20 <miguel.torres.20@hotmail.com>
2022-10-20 20:31:31 -0500

    Area method

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -63,3 +63,7 @@ class Rectangle(Base):
         elif value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    """ Area method """
+    def area(self):
+        return self.__width * self.__height
