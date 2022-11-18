19b64907f7aa37fb24d24812d9d11148db6e6729
MiguelGit20 <miguel.torres.20@hotmail.com>
2022-11-17 20:19:31 -0500

    Filter states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa.
+Script that lists all states with a name starting with N
+(upper N) from the database hbtn_0e_0_usa.
 """
 from sys import argv
 import MySQLdb
