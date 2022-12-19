9bd6ecf8e878595acf9c7ccbdfdd9d86461bb61d
MiguelGit20 <miguel.torres.20@hotmail.com>
2022-12-19 12:14:41 -0500

    Arguments

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+import process from 'process';
+const nArgs = process.argv.length - 2;
+if (nArgs === 0) {
+  console.log('No argument');
+} else if (nArgs === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
