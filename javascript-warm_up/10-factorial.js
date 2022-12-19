de78c7b01cd370f44cdc1818c36386cda1a0c1d9
MiguelGit20 <miguel.torres.20@hotmail.com>
2022-12-19 18:30:41 -0500

    Factorial

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -3,7 +3,7 @@
 const arg = process.argv;
 
 function factorialize (num) {
-  if (!num) { return 1; } else if (num < 0) { return -1; } else if (num == 0) { return 1; } else {
+  if (!num) { return 1; } else if (num < 0) { return -1; } else if (num === 0) { return 1; } else {
     return (num * factorialize(num - 1));
   }
 }
