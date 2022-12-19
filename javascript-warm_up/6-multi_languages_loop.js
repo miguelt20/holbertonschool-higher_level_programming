afe0e71de86d369bd56305f76423123c4d4378ae
MiguelGit20 <miguel.torres.20@hotmail.com>
2022-12-19 16:52:44 -0500

    Loop to languages

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-
-const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-for (let index = 0; index < languages.length; index++) { console.log(languages[index]); }
+const args = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let index = 0; index < args.length; index++) {
+  console.log(args[index]);
+}
