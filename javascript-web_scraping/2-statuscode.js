e15362c355855a7540ea1e3bdb007843ee4d200f
MiguelGit20 <miguel.torres.20@hotmail.com>
2022-12-22 18:07:31 -0500

    2-statuscode.js

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const request = require('request');
+const args = process.argv;
+
+request.get(args[2]).on('response', function (response) {
+  console.log('code:', response.statusCode);
+});
