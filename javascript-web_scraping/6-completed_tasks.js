05249494f50b56e023c4d4c40f1a65c254a12182
MiguelGit20 <miguel.torres.20@hotmail.com>
2022-12-23 00:01:01 -0500

    How many completed?

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+const args = process.argv;
+const url = args[2];
+
+function callback (error, response, body) {
+  if (!error || response.statusCode === 200) {
+    const data = JSON.parse(body);
+    const dic = {};
+    for (const d of data) {
+      dic[d.userId] = 0;
+    }
+    for (const d of data) {
+      if (d.completed === true) { dic[d.userId] += 1; }
+    }
+    console.log(dic);
+  }
+}
+
+request(url, callback);
