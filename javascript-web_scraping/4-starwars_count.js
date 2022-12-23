d3d711c1aadae91a9e7bfd9342880a53c9cae0f9
MiguelGit20 <miguel.torres.20@hotmail.com>
2022-12-22 20:33:18 -0500

    Star wars Wedge Antilles

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -3,7 +3,6 @@ const args = process.argv;
 const request = require('request');
 
 const url = args[2];
-const wedgeAntilles = 'https://swapi-api.hbtn.io/api/people/18/';
 
 function callback (error, response, body) {
   if (!error && response.statusCode === 200) {
@@ -11,7 +10,7 @@ function callback (error, response, body) {
     let count = 0;
     for (const item of data.results) {
       for (const i of item.characters) {
-        if (i === wedgeAntilles) {
+        if (i.includes('18')) {
           count += 1;
         }
       }
